4003e902ac5fcd417398d686fbdc0919124f9f2d
Michael Barto <michael.d.barto@gmail.com>
2024-04-29 01:59:51 +0000

    responds to single word input

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 import long_responses as long
 
-def message_probability(user_message, recognised_words, single_response=False, required_words=[])
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words = True
 
@@ -23,9 +23,25 @@ def message_probability(user_message, recognised_words, single_response=False, r
         return int(percentage*100)
     else:
         return 0
+    
+def check_all_messages(message):
+    highest_prob_list = {}
 
-    def get_response(user_input):
-        split_message = re.split(r'\s+I[,;?!.-]\s*', user_input.lower)
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+# Responses -------------------------------------------------------------------------------------------
+    response('Hello', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    print(highest_prob_list)
+        
+    return best_match
+
+def get_response(user_input):
+        split_message = re.split(r'\s+I[,;?!.-]\s*', user_input.lower())
         response = check_all_messages(split_message)
         return response
 
